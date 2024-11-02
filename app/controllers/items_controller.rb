052aea78f2984e046e60d3f13f59de07ed3386f8
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-11-02 18:07:11 +0000

    urrrrr

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @list_of_items = Item.all.order({ :created_at => :asc})
     render({ :template => "item_templates/list"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "zebra", :action => 
-giraffe })
+  get("/", { :controller => "items", :action => 
+"index" })
 end
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
   
+
